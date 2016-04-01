dbecd810940fe34fd864240a6cb46cb071003376
tinalora <tinagertsch@gmail.com>
2016-04-01 10:38:17 -0400

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -11,7 +11,7 @@ data <- read.table("/Users/tinalora/Downloads/household_power_consumption.txt", 
 namevar <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage",
              "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 
-colnames(data) <- namevar
+colnames(data) <- name
 
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
 
